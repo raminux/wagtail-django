31065f50db41133c4fe51fa559e8c81555bb8628
Ramin Esmzad <ramin.esmzad@hotmail.com>
2022-07-20 17:52:09 +0430

    How to install wagtail

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Wagtail Django Tutorial
+
+## How to install
+```bash
+$> mkdir wagtail-django
+$> cd wagtail-djago
+$> pipenv shell
+$> pip install wagtail
+$> pip freeze | grep wagtail
+$> wagtail start mysite
+$> mv mysite site
+$> cp -r site/* .
+$> rm -rf site
+$> pip install -r requirements.txt
+$> python manage.py migrate
+$> python manage.py createsuperuser
+$> python manage.py runserver
+```
+
+## Getting Started with First Home Page in Wagtail CMS
+
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-6t)pd-*ptowp25^6edhhf(pjz=ys_*v#8k!*ad=oi6mh2ojeqm"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
